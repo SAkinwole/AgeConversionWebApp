b3ddce87cc28f14fc9e3ad2516b6274d66e7756a
DevFlamez <samuelakinwole@gmail.com>
2023-07-08 06:44:17 +0100

    Add project files.

---
+++ AgeConversionWebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using AgeConversionWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AgeConversionWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateAge(int day, int month, int year)
+        {
+            DateTime birthDate = new DateTime(year, month, day);
+            DateTime currentDate = DateTime.Now;
+
+            int age = currentDate.Year - birthDate.Year;
+
+            // Check if the birth date hasn't occurred yet in the current year
+            if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+            {
+                age--;
+            }
+
+            ViewBag.Age = age;
+
+            return View("Index");
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AgeConversionWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgeConversionWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
